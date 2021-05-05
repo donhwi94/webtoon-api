00fa4abd13a61555abe5f51711114ebef132b6ac
donhwi94 <ehsgnl123@naver.com>
2021-05-05 21:08:08 +0900

    댓글 목록 조회, 댓글 등록, 댓글 삭제 기능 구현

---
+++ comments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.CommentList.as_view()),
+    path('<int:comment_id>/', views.CommentDetail.as_view()),
+]
--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView 
+
+from .models import Comment
+from .serializers import CommentSerializer
+from webtoons.models import Webtoon
+
+class CommentList(APIView):
+    def get(self, request, webtoon_id, episode_id, format=None):
+        comments = Comment.objects.filter(episode_info_id=episode_id)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, webtoon_id, episode_id, format=None):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(episode_info_id=episode_id, writer=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CommentDetail(APIView):
+    def get_object(self, webtoon_id, episode_id, comment_id):
+        return get_object_or_404(Comment, pk=comment_id, episode_info_id=episode_id)
+    
+    def get(self, request, webtoon_id, episode_id, comment_id, format=None):
+        comment = self.get_object(webtoon_id, episode_id, comment_id)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    
+    def delete(self, request, webtoon_id, episode_id, comment_id, format=None):
+        comment = self.get_object(webtoon_id, episode_id, comment_id)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
