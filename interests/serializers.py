82b51373fa1eab5c8f69f2d0783c2febcf1bf107
donhwi94 <ehsgnl123@naver.com>
2021-05-06 01:24:17 +0900

    관심 웹툰 등록, 관심 웹툰 목록 조회, 관심 웹툰 삭제 기능 구현

---
+++ interests/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from .models import Interest
+
+class InterestSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    class Meta:
+        model = Interest
+        fields = ['id', 'owner', 'interest_webtoon_list']
+
+class InterestDetailSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    interest_webtoon_list = serializers.ReadOnlyField(source='interest_webtoon_list.title')
+    class Meta:
+        model = Interest
+        fields = ['id', 'owner', 'interest_webtoon_list']
