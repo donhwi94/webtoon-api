5490b9f936cd6e6a7ec5b2334bd9397ee6dc6e94
donhwi94 <ehsgnl123@naver.com>
2021-05-05 21:08:08 +0900

    댓글 목록 조회, 댓글 등록, 댓글 삭제 기능 구현

---
+++ comments/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from .models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    episode_info = serializers.ReadOnlyField(source='episode_info.title')
+    writer = serializers.ReadOnlyField(source='writer.username')
+    class Meta:
+        model = Comment
+        fields = ['id', 'episode_info', 'writer', 'contents', 'created_at', 'likes', 'dislikes']
+
