d27d7584553f017d334483f08c55948b6d40d27e
chimback <77915239+chimback@users.noreply.github.com>
2021-05-09 14:01:06 +0900

    auto format 툴 사용하기
    https://pypi.org/project/black/

--- webtoons/urls.py
+++ webtoons/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.WebtoonList.as_view()),
+    path("", views.WebtoonList.as_view()),
     path('<int:webtoon_id>/episodes/', views.WebtoonDetail.as_view()),
     path('<int:webtoon_id>/episodes/<int:episode_id>/', views.EpisodeDetail.as_view()),
     path('<int:webtoon_id>/episodes/<int:episode_id>/comments/', include('comments.urls')),
