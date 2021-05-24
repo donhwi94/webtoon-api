53b737999dd868fa473f79a7e7df78a67b3ea728
donhwi94 <ehsgnl123@naver.com>
2021-05-24 18:20:26 +0900

    AWS EC2 배포 - settings 변경

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", ".ap-northeast-2.compute.amazonaws.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
