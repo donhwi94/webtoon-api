c4185f7e648fc6930c8c6afa89d0b069fb0b092a
donhwi94 <ehsgnl123@naver.com>
2021-06-06 17:08:46 +0900

    운영 환경 설정

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings.prod")
+
+project_folder = os.path.expanduser("/home/ubuntu/projects/webtoon-api")
+load_dotenv(os.path.join(project_folder, ".env"))
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings.prod")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
